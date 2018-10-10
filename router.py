59e1fbe902a8ac1b30f5c57eab0e1a53a2512364
Macmod <zz.mcmd@gmail.com>
2018-10-09 22:01:48 -0300

    Adding info on message send.

--- router.py
+++ router.py
@@ -24,8 +24,11 @@ def send_message(dest, msg):
         # Load balancing
         gateway = random.choice(gateways)
 
+        msg_str = str(msg).encode()
+        logging.info('Sending message to ' + gateway + ': ' + msg_str)
+
         # Send
-        sock.sendto(str(msg).encode(), (gateway, UDP_PORT))
+        sock.sendto(msg_str, (gateway, UDP_PORT))
 
 def handle_command(inp):
     cmdline = inp.split()
