f84a30f17b5d3fb4698afb9609db6e34086f1b2d
Macmod <zz.mcmd@gmail.com>
2018-10-12 09:53:19 -0300

    Changing tests.

--- scripts/fish-asymmetric/tmux.sh
+++ scripts/fish-asymmetric/tmux.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -eu
 
-exe="python3.5 ../../router.pyc"
+# exe="python3.5 ../../router.pyc"
 exe="../../router.py"
 
 for i in $(seq 1 6) ; do
-    tmux split-pane -v $exe --addr "127.0.1.$i" --startup-commands "$i.txt" &
+    tmux split-pane -v $exe "127.0.1.$i" 10 "$i.txt" &
     tmux select-layout even-vertical
 done
