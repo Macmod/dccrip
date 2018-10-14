265b6d1b4ecaaf7f4ebaf87932f536edef1bf3d1
Macmod <zz.mcmd@gmail.com>
2018-10-14 15:03:09 -0300

    Fixing mesh.

--- tests/mesh.sh
+++ tests/mesh.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
-# Setup window
-tmux new-session -d -s sample 'python3.5 ./router.py 127.0.0.1 1 tests/mesh/1.txt'
-
-tmux select-window -t sample:0
+tmux new-session -x 2000 -y 2000 -d -s sample './router.py 127.0.0.1 1 tests/mesh/1.txt'
 tmux rename-window 'Mesh Topology'
 
-tmux split-window -h -t 0 "python3.5 ./router.py 127.0.0.2 1 tests/mesh/2.txt"
+tmux split-window -v "./router.py 127.0.0.2 1 tests/mesh/2.txt"
+tmux split-window -v "./router.py 127.0.0.3 1 tests/mesh/3.txt"
 
-tmux split-window -h -t 1 "python3.5 ./router.py 127.0.0.3 1 tests/mesh/3.txt"
+tmux split-window -v "./router.py 127.0.0.4 1 tests/mesh/4.txt"
+tmux split-window -v "./router.py 127.0.0.5 1 tests/mesh/5.txt"
+tmux split-window -v "./router.py 127.0.0.6 1 tests/mesh/6.txt"
 
-tmux split-window -v -t 0 "python3.5 ./router.py 127.0.0.4 1 tests/mesh/4.txt"
-tmux split-window -v -t 1 "python3.5 ./router.py 127.0.0.5 1 tests/mesh/5.txt"
-tmux split-window -v -t 2 "python3.5 ./router.py 127.0.0.6 1 tests/mesh/6.txt"
+tmux split-window -v "./router.py 127.0.0.7 1 tests/mesh/7.txt"
+tmux split-window -v "./router.py 127.0.0.8 1 tests/mesh/8.txt"
+tmux split-window -v "./router.py 127.0.0.9 1 tests/mesh/9.txt"
 
-tmux split-window -v -t 3 "python3.5 ./router.py 127.0.0.7 1 tests/mesh/7.txt"
-tmux split-window -v -t 4 "python3.5 ./router.py 127.0.0.8 1 tests/mesh/8.txt"
-tmux split-window -v -t 5 "python3.5 ./router.py 127.0.0.9 1 tests/mesh/9.txt"
-
+tmux select-layout tiled
 # tmux send-keys -t 4 'update' 'C-m'
 
 sleep 2
@@ -27,4 +24,3 @@ for i in {0..8}; do
 done
 
 tmux -2 attach-session -t sample
-
