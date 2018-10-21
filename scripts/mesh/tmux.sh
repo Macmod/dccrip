bab557ffdf6fbb198c98c637ac4c023eb395d41e
Macmod <zz.mcmd@gmail.com>
2018-10-21 12:23:05 -0300

    Changing user-defined scripts and adding Pipfile.

--- scripts/mesh/tmux.sh
+++ scripts/mesh/tmux.sh
@@ -4,7 +4,7 @@ exe='./router.py'
 startup='scripts/mesh'
 
 # Setup window
-tmux new-session -x 2000 -y 2000 -d -s sample "$exe --addr 127.0.0.1 --update-period 6 --startup-commands $startup/1.txt"
+tmux new-session -x 2000 -y 2000 -d -s mesh "$exe --addr 127.0.0.1 --update-period 6 --startup-commands $startup/1.txt"
 tmux rename-window 'Mesh Topology'
 
 for i in $(seq 2 9); do
@@ -21,4 +21,4 @@ for i in {0..8}; do
 done
 
 # Attach
-tmux -2 attach-session -t sample
+tmux -2 attach-session -t mesh
